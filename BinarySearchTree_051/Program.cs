fcbfe30ed9451ce8e90e8e760f1e20cd6bc44f26
Prima06112002 <ramadhan06112002@gmail.com>
2022-12-30 17:53:38 +0700

    add case 3 and 4

--- BinarySearchTree_051/Program.cs
+++ BinarySearchTree_051/Program.cs
@@ -32,7 +32,7 @@ namespace BinarySearchTree_051
         public void insert(string element)/* Insert a node in the binary search tree */
         {
             Node tmp, parent = null, currentNode = null;
-            Search(element, ref parent, ref currentNode);
+            search(element, ref parent, ref currentNode);
             if (currentNode != null) /*Check if the node to be inserted already inserted or not*/
             {
                 Console.WriteLine("Duplicate words not allowed");
@@ -144,6 +144,16 @@ namespace BinarySearchTree_051
                             x.inorder(x.ROOT);
                         }
                         break ;
+                    case '3':
+                        {
+                            x.preorder(x.ROOT);
+                        }
+                        break;
+                    case '4':
+                        {
+                            x.postorder(x.ROOT);
+                        }
+                        break;
                 }
             }
         }
