66a5b122f8d6cf6d7e01caa62879870b3a855dba
Prima06112002 <ramadhan06112002@gmail.com>
2022-12-30 17:14:39 +0700

    add public node left and rightchild

--- BinarySearchTree_051/Program.cs
+++ BinarySearchTree_051/Program.cs
@@ -8,7 +8,17 @@ namespace BinarySearchTree_051
 {
     class Node
     {
-        
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+
+        //Constructor for the node class
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
     }
     internal class Program
     {
